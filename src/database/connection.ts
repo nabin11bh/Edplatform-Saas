7016f57a731f13226412c9ee382c9d96c9789417
nabin11bh <nabinbhattarai214@gmail.com>
2025-05-29 20:16:01 +0545

    database connected

---
+++ src/database/connection.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+    database:process.env.DB_NAME,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host:process.env.DB_HOST,
+    dialect: "mysql",
+    port:  Number(process.env.DB_PORT)
+
+})
+
+sequelize.authenticate()
+.then((=>{
+    console.log("Authenticated,connected")
+}))
