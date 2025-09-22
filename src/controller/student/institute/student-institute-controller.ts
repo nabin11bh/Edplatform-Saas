06aaa6709f394853308435a62479a0ee9571a96f
nabin11bh <nabinbhattarai214@gmail.com>
2025-09-22 09:06:19 +0545

    student

--- src/controller/student/institute/student-institute-controller.ts
+++ src/controller/student/institute/student-institute-controller.ts
@@ -1,15 +1,49 @@
+import { Request, Response } from "express";
+import sequelize from "../../../database/connection";
+import { QueryTypes } from "sequelize";
 
-import { Request,Response } from "express"
-import sequelize from "../../../database/connection"
-import { table } from "console"
-import { QueryTypes } from "sequelize"
+
+
 
 const instituteListForStudent = async(req:Request,res:Response)=>{
+
+  const tables = await sequelize.query(`SHOW TABLES LIKE 'institute_%'`,{
     type : QueryTypes.SHOWTABLES
-    const tables =   await sequelize.query(`SHOW TABLES LIKE 'institute_%'`)
-    res.status(200).json({
-        message : " data fetched", data : tables
+  })
+//   console.log(tables,"Tables") institute_111017, institute_123123
+  let allDatas = []
+ for(let table of tables){
+    console.log(table)
+    // table --> institute_1234,institute_3434, 
+    const instituteNumber = table.split("_")[1]
+   const [data] =  await sequelize.query(`SELECT instituteName, institutePhoneNumber FROM ${table}`,{
+        type : QueryTypes.SELECT
     })
+
+    allDatas.push({instituteNumber : instituteNumber,...data})
+ }
+  res.status(200).json({
+    message : "data fetched", data : allDatas
+  })
+
+}
+const instituteCourseListForStudent = async(req:Request,res:Response)=>{
+    const {instituteId} = req.params
+    const datas = await sequelize.query(`SELECT co.id as courseId,co.courseName,co.courseDescription,co.coursePrice,cat.id,cat.categoryName FROM course_${instituteId} AS co JOIN category_${instituteId} AS cat ON co.categoryId = cat.id`,{
+        type : QueryTypes.SELECT
+    })
+    console.log(datas.length)
+    if(datas.length == 0){
+        res.status(404).json({
+            message : "No courses found of that institute"
+        })
+    }else{
+
+        res.status(200).json({
+            message : "Courses fetched", data : datas
+        })
+    }
 }
 
-export default instituteListForStudent
+
+export  {instituteListForStudent, instituteCourseListForStudent}
--- src/routes/student/institute/student-institute-route.ts
+++ src/routes/student/institute/student-institute-route.ts
@@ -1,7 +1,9 @@
 import express,{ Router } from "express";
 import asyncErrorHandling from "../../../services/asyncErrorHandling";
-import instituteListForStudent from "../../../controller/student/institute/student-institute-controller";
+import {instituteListForStudent} from "../../../controller/student/institute/student-institute-controller";
 
 const router:Router = express.Router()
 router.route("/institute").get(asyncErrorHandling(instituteListForStudent))
+router.route("/institute/:institutedId/courses").get(asyncErrorHandling(instituteListForStudent))
+
 export default router;
